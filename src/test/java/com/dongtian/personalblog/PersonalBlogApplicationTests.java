b0d33bbff5b46d5de7c63851fde8832b29c28e63
lindongtian <707493342@qq.com>
2020-05-12 16:06:55 +0800

    first commit

---
+++ src/test/java/com/dongtian/personalblog/PersonalBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongtian.personalblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
